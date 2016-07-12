73e5f348915faa45668f44fe92fb9d2207563d82
Andrew Alex <andrewalex123@ucla.edu>
2016-07-12 15:10:11 -0700

    added login functionality tester

--- myats/tests.py
+++ myats/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.test.utils import setup_test_environment
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class LoginTest(TestCase):
+    def test_login_failure(self):
+        setup_test_environment()
+        client = Client()
+        response = client.post('/login/', {'username': 'failed', 'password':'login'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'login.html')
+        self.assertContains(response,'Please try again.')
+
+    def test_login_success(self):
+        setup_test_environment()
+        client = Client()
+        response = client.get('/login/')
+        self.assertNotContains(response, 'Please try again.')
+        self.assertTemplateUsed(response,'login.html')
+        user = User.objects.create_user('test_user', password='test_password')
+        user.save()
+        response = client.post('/login/', {'username': 'test_user','password':'test_password'})
+        self.assertEqual(response.status_code,302)
+
+    def test_home_restricted(self):
+        setup_test_environment()
+        client = Client()
+        response = client.get('/home/')
+        self.assertEqual(response.status_code, 302)
+        user = User.objects.create_user('test_user', password='test_password')
+        user.save()
+        client.login(username='test_user',password='test_password')
+        response = client.get('/home/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        client.logout()
+        response = client.get('/home/')
+        self.assertEqual(response.status_code,302)
