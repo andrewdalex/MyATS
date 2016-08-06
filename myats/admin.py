8972d76c62b66094c5428d95c710ff15641bafb3
Andrew Alex <andrewalex123@ucla.edu>
2016-08-05 18:41:48 -0700

    updated the models to prepare for rendering form list on home page and altered student admin to update form receipt inline

--- myats/admin.py
+++ myats/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import *
-admin.site.register(Student)
+
+class ReceivedFormInline(admin.TabularInline):
+    model = ReceivedForm
+    extra = 1
+
+class StudentAdmin(admin.ModelAdmin):
+    inlines = (ReceivedFormInline,)
+
+
+admin.site.register(Student, StudentAdmin)
 admin.site.register(Class)
 admin.site.register(Resource)
+admin.site.register(Form)
 admin.AdminSite.site_header = 'ATS Administration'
 admin.AdminSite.site_title = 'ATS Administration'
 admin.AdminSite.site_url = '/home/'
--- myats/models.py
+++ myats/models.py
@@ -3,12 +3,22 @@ from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
 # Create your models here.
 
+class Form(models.Model):
+    form_name = models.CharField(max_length=150)
+    form_deadline = models.DateField()
+    class Meta:
+        verbose_name = "Form"
+        verbose_name_plural = "Forms"
+    def __str__(self):
+        return self.form_name
+
 class Student(models.Model):
     student_user = models.OneToOneField(User, null=True)
     student_grade = models.SmallIntegerField()
     student_first_name = models.CharField(max_length=50)
     student_last_name = models.CharField(max_length=50)
     student_school = models.CharField(max_length=100)
+    forms_recieved = models.ManyToManyField(Form, related_name='students_completed',through="ReceivedForm")
     class Meta:
         verbose_name_plural = 'Students'
         verbose_name = 'Student'
@@ -25,6 +35,21 @@ class Class(models.Model):
     class Meta:
         verbose_name_plural = 'Classes'
         verbose_name = 'Class'
+    def __str__(self):
+        return self.class_title
 
 class Resource(models.Model):
     upload = models.FileField()
+    resource_name = models.CharField(max_length=150, null=True)
+    class Meta:
+        verbose_name = "PDF"
+        verbose_name_plural = "PDFs"
+    def __str__(self):
+        return self.resource_name
+
+class ReceivedForm(models.Model):
+    form = models.ForeignKey(Form, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    date_recieved = models.DateField()
+    def __str__(self):
+        return '{0},{1}'.format(self.student.student_last_name,self.student.student_first_name)
