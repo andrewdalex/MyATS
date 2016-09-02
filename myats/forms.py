ea61ac7e74fdd4eb3738fce4642f68d5a5584664
Andrew Alex <andrewalex123@ucla.edu>
2016-09-01 22:53:30 -0700

    added search form validation and error message rendering

--- myats/forms.py
+++ myats/forms.py
@@ -1,4 +1,11 @@
 from django import forms
+from .models import Student
 
 class SearchIdForm(forms.Form):
     person_id = forms.IntegerField()
+    def clean(self):
+        cleaned_data = super(SearchIdForm, self).clean()
+        person_id = cleaned_data.get('person_id')
+        if not Student.objects.filter(student_id=person_id).exists():
+            raise forms.ValidationError('Invalid ID Number')
+        return cleaned_data
